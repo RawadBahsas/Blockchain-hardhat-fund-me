6b4b913d6fb5443759051521d99b31a55cf82e2e
RawadBahsas <rawadnb@gmail.com>
2023-03-25 12:09:03 +0400

    cleaning contract

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -2,30 +2,6 @@
 pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-// library PriceConverter {
-//     function getPrice(
-//         AggregatorV3Interface priceFeed
-//     ) internal view returns (uint256) {
-//         // ABI from npm interface
-//         //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-//         // AggregatorV3Interface priceFeed = AggregatorV3Interface(
-//         //     0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-//         // );
-//         (, int256 price, , , ) = priceFeed.latestRoundData();
-//         //int256 dec = int8(priceFeed.decimals());
-//         return uint256(price * (10 ** 8));
-//     }
-
-//     function getConversionRate(
-//         uint256 ethAmount,
-//         AggregatorV3Interface priceFeed
-//     ) internal view returns (uint256) {
-//         //int256 dec = 10 ^ int8(priceFeed.decimals());
-//         uint256 ethPrice = getPrice(priceFeed);
-//         uint256 ethAmountiInUsd = ((ethPrice * ethAmount) / 10000000000000000);
-//         return ethAmountiInUsd;
-//     }
-// }
 library PriceConverter {
     function getPrice(
         AggregatorV3Interface priceFeed
